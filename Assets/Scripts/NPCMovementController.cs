7c6fc36d5fb8dadd65feac3fe50734a1c1d5e94f
John Patrick Quintos <113108715+patqnts@users.noreply.github.com>
2024-03-19 15:25:30 +0800

    Katambay pull

--- Assets/Scripts/NPCMovementController.cs
+++ Assets/Scripts/NPCMovementController.cs
@@ -10,13 +10,22 @@ public class NPCMovementController : MonoBehaviour
 
     Rigidbody2D rb; // Reference to the Rigidbody2D component
 
+    private GameObject playerObject;
+    [Header("Chapter 1 USE")]
+    public Transform targetLocation;
+
     void Start()
     {
+        playerObject = player.gameObject;
         rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the NPC
+        
     }
-
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PullingScene();
+        }
         // Calculate the distance between the NPC and the player
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
@@ -35,6 +44,43 @@ public class NPCMovementController : MonoBehaviour
         // Move towards the player
         rb.velocity = direction * 2f;
     }
+    public void PullingScene()
+    {
+        // 2. Carry the player (make it its child object) above the NPC's head
+        player.SetParent(transform);
+        player.localPosition = new Vector3(0f, 1f, 0f); // Adjust the y position to make it appear above the NPC's head
+        player.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        // 3. Move the player to the target location after a delay
+        StartCoroutine(MovePlayerToTargetAfterDelay());
+    }
+
+    IEnumerator MovePlayerToTargetAfterDelay()
+    {
+        yield return new WaitForSeconds(1f); // Wait for 3 seconds
+
+        // Move the player to the target location
+        if (targetLocation != null)
+        {
+            // Unparent the player from the NPC
+            player = targetLocation;
+        }
+
+        StartCoroutine(Uncarry());
+    }
+
+    IEnumerator Uncarry()
+    {
+        yield return new WaitForSeconds(10f);
+        
+        
+        player = playerObject.transform;
+        playerObject.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        playerObject.transform.SetParent(null);
+        PixelCrushers.DialogueSystem.Sequencer.Message("SceneEnd");
+    }
+
+
+
 
     void SetAnimation(Vector2 direction)
     {
